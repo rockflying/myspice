392eddde0c68a23dc34910f3a92171ae6ece17ba
xuhao <hao.xu@cs2c.com.cn>
2014-09-26 16:22:53 +0800

    spice_log

--- src/log.c
+++ src/log.c
@@ -5,11 +5,47 @@
 /* not use this file except in compliance with the License. You may obtain  */
 /* a copy of the License at                                                 */
 /*                                                                          */
-/* http://www.apache.org/licenses/LICENSE-2.0                               */
+/* http://www.argsache.org/licenses/LICENSE-2.0                               */
 /*                                                                          */
-/* Unless required by applicable law or agreed to in writing, software      */
+/* Unless required by argsplicable law or agreed to in writing, software      */
 /* distributed under the License is distributed on an "AS IS" BASIS,        */
 /* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. */
 /* See the License for the specific language governing permissions and      */
 /* limitations under the License.                                           */
 /* ------------------------------------------------------------------------ */
+
+#include "log.h"
+
+static unsigned int log_level = 0;
+
+static FILE* log_file = NULL;  //stderr
+
+void spice_log(unsigned int level,
+		const char* strloc,
+		const char *function,
+		const char *format, ...)
+{
+    va_list args;
+
+    if (level < log_level) {
+      return;
+    }
+
+    //ASSERT(type <= LOG_FATAL);
+
+    //if (level) {
+    //    fprintf(stderr, "%s **: ", level);
+    //}
+    if (strloc && function) {
+        fprintf(stderr, "%s in %s: ", strloc, function);
+    }
+
+    va_start(args, format);
+    if (format) {
+		vfprintf(stderr, format, args);
+    }
+    va_end(args);
+
+    fprintf(stderr, "\n");
+
+}
